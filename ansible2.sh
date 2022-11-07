1df223dc73689880f8f38ba7b2db43a78cfa4704
Akalugo David <109105332+rigamortus@users.noreply.github.com>
2022-11-07 18:20:10 +0100

    Delete ansible2.sh

--- ansible2.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-sudo -i -u postgres psql -c "CREATE DATABASE laravel_realworld"
-sudo -i -u postgres psql -c "CREATE USER root WITH PASSWORD 'Phunkeeb100*'";
-sudo -i -u postgres psql -c "GRANT postgres TO root";
-
